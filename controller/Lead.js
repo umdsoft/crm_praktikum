29cabaa4df6cbe98139ad35225100e2bc28ee7d4
Umidbek <ujumaniyozov@ya.ru>
2024-03-29 21:23:02 +0500

    ASD

--- controller/Lead.js
+++ controller/Lead.js
@@ -1,17 +1,27 @@
 const Lead = require("../models/leads");
 const LeadInterested = require("../models/lead_interested");
 const Target = require("../models/target");
+const NewLead = require("../models/NewLead");
+const LeadAction = require("../models/lead_action");
 exports.create = async (req, res) => {
   try {
-    await Lead.query().insert({
-      name: req.body.full_name,
-      phone: req.body.phone,
-      target_id: req.body.target,
-      edit_date: new Date(),
-    });
+    await Lead.query()
+      .insert({
+        name: req.body.full_name,
+        phone: req.body.phone,
+      })
+      .then(async (lead) => {
+        await NewLead.query().insert({
+          lead_id: lead.id,
+          target_id: req.body.target,
+          edit_date: new Date(),
+          edit_time: new Date(),
+        });
+      });
+
     return res.status(200).json({ success: true });
   } catch (e) {
-    return console.log(e)
+    return console.log(e);
   }
 };
 exports.getTarget = async (req, res) => {
@@ -33,5 +43,46 @@ exports.getById = async (req, res) => {
     return res.status(200).json({ success: true, lead, interest });
   } catch (e) {
     return res.status(400).json({ success: false, msg: e });
+  }
+};
+
+exports.getByNew = async (req, res) => {
+  try {
+    const limit = req.query.limit || 10;
+    const skip = (req.query.page - 1) * limit || 0;
+    const action = req.query.action || 0;
+    const leads = await NewLead.knex().raw(`
+    SELECT nl.id,nl.created,nl.time,l.name,l.id as uid,l.phone,t.name as target FROM new_lead nl 
+    left join leads l on nl.lead_id = l.id
+    left join target t on nl.target_id = t.id
+    WHERE nl.action = ${action}
+    ORDER BY nl.id DESC
+    limit ${limit}
+    OFFSET ${skip};
+    `);
+    return res
+      .status(200)
+      .json({ success: true, leads: leads[0], total: leads[0].length, limit });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+exports.editAction = async (req, res) => {
+  try {
+    const newLead = await NewLead.query().findById(req.params.id);
+    await NewLead.query().patchAndFetchById(req.params.id, {
+      action: req.body.action,
+      edit_date: new Date(),
+      edit_time: new Date(),
+    });
+    await LeadAction.query().insert({
+      lead_id: req.params.id,
+      to: newLead.to,
+      do: req.body.action
+    });
+    return res.status(200).json({ success: true });
+  } catch (e) {
+    console.log(e);
   }
 };
--- router/router/lead.js
+++ router/router/lead.js
@@ -2,7 +2,10 @@ const Lead = require("../../controller/Lead");
 const router = require("express").Router();
 const authMiddleware = require("../../middleware/auth");
 
-router.get('/get-target', Lead.getTarget)
-router.post('/create', Lead.create)
+router.get("/get-target", Lead.getTarget);
+router.post("/create", Lead.create);
+
+router.put("/edit-action/:id", Lead.editAction);
+router.get("/get-lead", Lead.getByNew);
 
 module.exports = router;
