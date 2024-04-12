6a8f2f96684864eff6ec10a0e0c31bf7bfdcb18b
Umidbek <ujumaniyozov@ya.ru>
2024-04-12 17:21:07 +0500

    ASD

--- router/router/user.js
+++ router/router/user.js
@@ -2,13 +2,6 @@ const UserController = require("../../controller/User");
 const router = require("express").Router();
 const authMiddleware = require("../../middleware/auth");
 const { role } = require("../../middleware/role");
-const limitter = require("express-rate-limit");
-
-const registerLimit = limitter({
-  windowMs: 2 * 60 * 1000,
-  max: 3,
-  message: "Siz ko'p so'rov yuboryabsiz, iltimos biroz kuting!",
-});
 
 router.post("/register", authMiddleware, UserController.register);
 router.post("/login", UserController.login);
