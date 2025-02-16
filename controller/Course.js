e29aa16583d0e49ec240b618d4be348c29457bc8
Umidbek <ujumaniyozov@ya.ru>
2025-02-16 20:40:00 +0500

    AD

--- controller/Course.js
+++ controller/Course.js
@@ -1,10 +1,33 @@
-const Course = require('../models/Direction')
-
+const Course = require("../models/Direction");
+const Module = require("../models/LessonModule");
 exports.getAllCourse = async (req, res) => {
-    try {
-        const courses = await Course.query().select('*');
-        res.status(200).json({ success: true, courses });
-    } catch (error) {
-        res.status(500).json({ success: false, msg: "Internal server error" });
-    }
-}
+  try {
+    const courses = await Course.query().select("*");
+    res.status(200).json({ success: true, courses });
+  } catch (error) {
+    res.status(500).json({ success: false, msg: "Internal server error" });
+  }
+};
+exports.createModule = async (req, res) => {
+  try {
+    const { module_name, course_id } = req.body;
+    await Module.query().insert({
+      name: module_name,
+      direction_id: course_id,
+      status: 1,
+      created: new Date(),
+    });
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(error);
+    //res.status(500).json({ success: false, msg: "Internal server error" });
+  }
+};
+exports.getModule = async (req, res) => {
+  try {
+    const modules = await Module.query().select("*").where("direction_id", req.params.id);
+    return res.status(200).json({ success: true, modules });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- router/router/course.js
+++ router/router/course.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const authMiddleware = require("../../middleware/auth");
 
 router.get("/get-all", authMiddleware, Course.getAllCourse);
-
+router.post("/create-module", authMiddleware, Course.createModule);
+router.get("/get-module/:id", authMiddleware, Course.getModule);
 module.exports = router;
