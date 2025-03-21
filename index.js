990fb7525903a12e3c9bea920bad3a0d2677780b
Umidbek Jumaniyozov <ujumaniyozov@Umidbeks-iMac.local>
2025-03-21 09:05:15 +0500

    ASD

--- index.js
+++ index.js
@@ -25,18 +25,11 @@ app.use(
     },
   })
 );
-// Configure CORS
-app.use(cors({
-  origin: 'http://localhost:3000', // Specify the frontend origin
-  credentials: true, // Allow credentials (cookies, etc.)
-  methods: ['GET', 'POST', 'OPTIONS'], // Allow these methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-}));
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// Handle preflight OPTIONS requests
-app.options('*', cors());
+app.use(cors("*"));
 app.use(morgan("tiny"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api", require("./router/index"));
